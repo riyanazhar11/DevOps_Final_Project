338cf7082c90061377da75015c9869e7ad9eb091
riyanazhar11 <riyanazhar10@gmail.com>
2024-12-08 21:05:39 +0500

    terraform files updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,11 @@ module "Network" {
   RDS_subnets_cidr = var.RDS_subnets_cidr
   az               = var.az
 }
-
-
 module "SG" {
   source    = "./SG"
   vpc_id    = module.Network.vpc_id
   sg-values = var.sg-values
 }
-
 module "EKS" {
   source         = "./EKS"
   cluster_name   = var.cluster_name
@@ -21,29 +18,23 @@ module "EKS" {
   sg_id          = module.SG.sg_id
   nodes_configs  = var.nodes_configs
 }
-
 module "ECR" {
   source   = "./ECR"
   ecr_info = var.ecr_info
 }
-
 # module "secret-manager" {
 #   source = "./secret-manager"
-
 # }
-
 module "RDS" {
   source         = "./RDS"
   RDS_subnets_id = module.Network.RDS_subnets_id
   sg_id          = module.SG.sg_id
   rds_info       = var.rds_info
 }
-
 module "oidc_issuer_url" {
   source         = "./oidc-role"
   eks_issuer_url = module.EKS.oidc_issuer_url
 }
-
 module "K8S-oprators" {
   source     = "./K8S-oprator"
   namespaces = var.namespaces
